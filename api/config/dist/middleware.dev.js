048091f37436f9a710c5cb52096b1b3a483f7537
Sumsam Ali <freelancingwithsac@gmail.com>
2024-05-30 14:25:28 +0500

    updated middleware

--- api/config/dist/middleware.dev.js
+++ api/config/dist/middleware.dev.js
@@ -11,10 +11,23 @@ var _helmet = _interopRequireDefault(require("helmet"));
 
 var _express = _interopRequireDefault(require("express"));
 
+var _dotenv = require("dotenv");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+(0, _dotenv.config)();
+process.env.VITE_WS_URL;
+
 function setupMiddlewares(app) {
-  app.use((0, _helmet["default"])());
+  app.use((0, _helmet["default"])({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        connectSrc: ["'self'", "ws://localhost:8080"] // Adjust according to your needs
+
+      }
+    }
+  }));
   app.use((0, _cors["default"])());
   app.use(_express["default"].json());
   app.use(_express["default"]["static"]("public"));
